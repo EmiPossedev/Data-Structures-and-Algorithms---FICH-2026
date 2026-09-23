28780aaecc3dece99622ca9b05bdfdeebe2b9065
EmiPossedev <posseemiliano2@gmail.com>
2026-09-23 17:27:22 -0300

    Refactor concatenation functions for lists and add missing implementation for nested lists

--- List/Ej 3 Concatenar Listas.cpp
+++ List/Ej 3 Concatenar Listas.cpp
@@ -2,6 +2,21 @@
 #include <list>
 using namespace std;
 
+void mostrar_lista(const list<int> &L) // una función para mostrar mi lista
+{
+    cout << "{";
+
+    for (auto it = L.begin(); it != L.end(); ++it)
+    {
+        cout << *it;
+        if (next(it) != L.end())
+        {
+            cout << ", ";
+        }
+    }
+    cout << "}" << endl;
+}
+
 /* Concatena. Escriba procedimientos para concatenar:
     a) dos listas L1 y L2 usando insert;
     b) una lista LL de n sublistas usando insert;
@@ -17,13 +32,36 @@ list<int> concatenar_2_listas_insert(list<int> &l1, list<int> &l2) // dos listas
     return resultado;
 }
 
-list<list<int>> concatenar_2_listas_anidadas(list<list> &l1, list<int> &l2)
+list<int> concatenar_2_listas_sublista_insert(list<list<int>> &ll) // una lista de n sublistas usando insert
 {
-    
+    list<int> resultado;
+
+    for (auto it = ll.begin(); it != ll.end(); it++)
+    {
+        resultado.insert(resultado.end(), it->begin(), it->end());
+    }
+
+    return resultado;
 }
+
+list<int> concatenar_lista_sublista_splice(list<list<int>> &ll, )
 
 int main()
 {
+    // inciso a: concatenar dos listas de int con insert
+    list<int> l1 = {1, 2, 3};
+    list<int> l2 = {4, 5, 6};
+    list<int> aux1 = concatenar_2_listas_insert(l1,l2);
+    mostrar_lista(aux1);
+
+    // inciso b_ concatenar una lista de sublistas de int usando insert
+    list<int> l3 = {7, 8, 9};
+    list<int> l4 = {10, 11, 12};
+    list<list<int>> l5 = {l3,l4};
+    list<int> aux2 = concatenar_2_listas_sublista_insert(l5);
+    mostrar_lista(aux2);
+
+
 
     return 0;
 }
